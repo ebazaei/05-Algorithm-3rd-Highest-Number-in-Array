6523cc7b40f0ad2e9da46082a925f99af8a020f5
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-27 12:34:37 +0100

    2th sol

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 // Using the array [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31], the correct answer is : 11
 
 
+
+//Solotion 1
+
 let Array1 = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
 console.log(`Original Array is: ${Array1}`);
 //Variable for 3rd nummer
@@ -25,8 +28,22 @@ function num3rd(numOutput){
 
 console.log(`Using the array ${Array1}, the correct answer is : ${num3rd()}`)
 
+console.log("______________________________________________________________")
 
 
+
+//Solotion 2
+
+let Array1b = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
+console.log(`Original Array B is: ${Array1b}`);
+
+let Array2b = [...Array1].sort((a, b) => a - b);
+console.log(`Sorted Array B is: ${Array2b}`);
+var num1b = 0
+for( i = Array2b.length ; i >= Array2b.length - 3 ; i-- ){
+    var num1b = Array2b[i];
+}
+console.log(`Using the array B ${Array1b}, the correct answer is : ${num1b}`)
 
 
 
